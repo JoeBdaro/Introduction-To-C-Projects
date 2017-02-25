9f92c2f2b32247f83cf441b08c83b7066b6c8d6d
Joseph Bdaro <emaster94@gmail.com>
2017-02-24 21:19:26 -0500

    Add files via upload

---
+++ n_Squared/Main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	double num;
+
+	//prompt and input palue of n
+
+	cout << "Input a number and press ENTER: ";
+	cin >> num;
+
+	//Calculate and output the square
+	cout << "The Square is: " << num * num << endl;
+
+	system("PAUSE");
+	return 0;
+}
