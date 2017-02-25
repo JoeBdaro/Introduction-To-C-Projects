3a7adbbc517c76f828a501827b937e3001b0f548
Joseph Bdaro <emaster94@gmail.com>
2017-02-24 21:17:15 -0500

    Add files via upload

---
+++ n_Cubed/Main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	double n;
+
+	// Prompt and value of n.
+
+	cout << "Input a number and press ENTER to find the cube of that number: ";
+	cin >> n;
+
+
+
+	// Calculate and output the square.
+
+	cout << "The Cube is: " << n * n * n << endl;
+
+	system("PAUSE");
+	return 0;
+}
